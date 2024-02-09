5d2e7412e9e39d737206ccc9c3a28c20f8f9c618
AdrianKallo <97944193+AdrianKallo@users.noreply.github.com>
2024-02-09 15:57:05 +0200

    09.02

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// index.js
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const rental = require('./rentalPrice');
@@ -16,15 +18,23 @@ const resultHtml = fs.readFileSync('result.html', 'utf8');
 
 app.post('/', (req, res) => {
     const post = req.body;
-    const result = rental.price(
+
+    // Extracting driver's license duration from input
+    const licenseStartDate = new Date(post.licensedate);
+    const currentDate = new Date();
+    const licenseDurationInYears = (currentDate - licenseStartDate) / (1000 * 60 * 60 * 24 * 365);
+
+    const result = rental.calculateRentalPrice(
         String(post.pickup),
         String(post.dropoff),
         Date.parse(post.pickupdate),
         Date.parse(post.dropoffdate),
         String(post.type),
-        Number(post.age)
+        Number(post.age),
+        licenseDurationInYears // Pass license duration to rental price calculation function
     );
-    res.send(formHtml + resultHtml.replaceAll('$0', result));
+
+    res.send(formHtml + generateResultHtml(result));
 });
 
 app.get('/', (req, res) => {
@@ -35,3 +45,24 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
+
+// Function to generate HTML for displaying rental price result
+function generateResultHtml(result) {
+    let html = `<ul class="car-list">`;
+
+    for (const [key, value] of Object.entries(result)) {
+        if (key === 'error') {
+            html += `<li class="error-message">${value}</li>`;
+        } else {
+            html += `<li class="car-item">
+                        <div class="car-details">
+                            <div class="car-name">${key}</div>
+                            <div class="car-price">Price: $${value.toFixed(2)} per day</div>
+                        </div>
+                    </li>`;
+        }
+    }
+    
+    html += `</ul>`;
+    return html;
+}
